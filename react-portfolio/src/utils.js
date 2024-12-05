0624f9d8f745f3dffd6ff543ac041e51e1fe3358
Alex2898 <stepy.go94@gmail.com>
2024-12-05 18:27:14 +0100

    Deleting all

--- react-portfolio/src/utils.js
+++
@@ -1,9 +0,0 @@
-export const getImageUrl = (path) => {
-    // console.log(path)
-    // console.log(new URL(`./assets/`+path, import.meta.url).href);
-
-    return new URL(`./assets/`+path, import.meta.url).href;
-} //take the relative path, save base url of website, append the relative
-//console.log(new URL(`/assets/nav/menuIcon.png`, import.meta.url).href);
-
-
